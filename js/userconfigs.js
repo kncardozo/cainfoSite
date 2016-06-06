42183cf0a9b9c780086df913c4dd60b68164133d
karine.do.nascimento.cardozo <karine.do.nascimento.cardozo@7d8fc066-4b3c-cced-95b8-ce57b7db7bff>
2016-06-06 20:28:57 +0000

    Primeiro
    git-svn-id: https://github.com/kncardozo/cainfoSite.git/trunk@1 7d8fc066-4b3c-cced-95b8-ce57b7db7bff

---
+++ js/userconfigs.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Desktop on 13/05/2016.
+ */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
